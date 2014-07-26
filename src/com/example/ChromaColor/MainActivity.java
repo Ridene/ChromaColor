31e5d7cbb8d3e51ebe65a2850b5a8809b94b7487
bouhanef <bouhanef.h@sfeir.com>
2014-07-26 20:52:32 +0200

    Renaming java main file and lyout main file name

--- src/com/example/ChromaColor/MainActivity.java
+++ src/com/example/ChromaColor/MainActivity.java
@@ -3,13 +3,13 @@ package com.example.ChromaColor;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MyActivity extends Activity {
+public class MainActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main_layout);
     }
 }
